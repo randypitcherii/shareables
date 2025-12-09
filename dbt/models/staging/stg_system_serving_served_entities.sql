6bef28985c1f5ffb820e5b3ff40424daed0cd695
Randy Pitcher <randypitcherii@gmail.com>
2025-12-09 14:09:37 -0500

    forgot to commit the deletes

--- dbt/models/staging/stg_system_serving_served_entities.sql
+++
@@ -1,9 +0,0 @@
-select
-    endpoint_id,
-    endpoint_name,
-    endpoint_config_version,
-    created_by,
-    workspace_id,
-    change_time,
-    endpoint_delete_time
-from {{ source('system_serving', 'served_entities') }} 
