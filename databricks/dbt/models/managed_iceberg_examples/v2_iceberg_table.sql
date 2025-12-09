13bd1a265b09fd14611d9c3cce58012af1b5ce3f
Randy Pitcher <randypitcherii@gmail.com>
2025-12-09 14:07:17 -0500

    dbt init with v2 and v3 managed iceberg table examples.

---
+++ databricks/dbt/models/managed_iceberg_examples/v2_iceberg_table.sql
@@ -0,0 +1,9 @@
+{{ config(
+    tblproperties={
+      'format-version': 2
+    }
+ ) }}
+
+select 
+  1 as id,
+  'v2 iceberg table' as name
---
+++ databricks/dbt/models/managed_iceberg_examples/v3_iceberg_table.sql
@@ -0,0 +1,3 @@
+select 
+  1 as id,
+  'v3 iceberg table' as name
