07e8162fb4635fde2d2514382301b8da17ca07ae
Randy 🌞 <randypitcherii@gmail.com>
2026-08-17 15:22:10 -0400

    Add 'whoops' column to cost_by_product
    Added a new column 'whoops' that divides 1 by 0, which may cause an error.

--- databricks/demos/dbt/models/marts/finance/cost_by_product.sql
+++ databricks/demos/dbt/models/marts/finance/cost_by_product.sql
@@ -6,6 +6,7 @@ select
     currency_code,
     count(distinct workspace_id) as n_workspaces,
     sum(usage_quantity)          as usage_quantity,
-    sum(list_cost)               as list_cost
+    sum(list_cost)               as list_cost,
+    1/0 as whoops
 from {{ ref('int_usage_priced') }}
 group by billing_origin_product, usage_date, cloud, currency_code
